fe3c953f61773d74b1c30883cc6297320165673f
smokrala <114281677+smokrala@users.noreply.github.com>
2023-07-03 18:30:39 -0400

    Add files via upload

---
+++ Namaste india/js/script.js
@@ -0,0 +1,15 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active');
+
+}
+
+let ShoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+    ShoppingCart.classList.toggle('active');
+
+}
